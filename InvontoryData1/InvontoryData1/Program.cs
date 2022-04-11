425e90ea454322747646341e2dbcb76928d44696
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-11 17:25:22 +0530

    Added Inventory data management

---
+++ InvontoryData1/InvontoryData1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            InventoryManager manager = new InventoryManager();
+            string file = @"Inventory.json";
+            //InventoryUtility utility = JsonContent.Create<InventoryUtility>(File.ReadAllText(file));
+            InventoryUtility utility = JsonConvert.DeserializeObject<InventoryUtility>(File.ReadAllText(file));
+            Console.WriteLine("Display inventory Press 1-Rice Inventory 2-Wheat Inventory 3-Pulse Inventory ");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    manager.DisplayInventory(utility.RiceList);
+                    break;
+                case 2:
+                    manager.DisplayInventory(utility.WheatList);
+                    break;
+                case 3:
+                    manager.DisplayInventory(utility.PulseList);
+                    break;
+            }
+        }
+    }
+}
