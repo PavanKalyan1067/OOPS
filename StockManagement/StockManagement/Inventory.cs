cda9dcde822e7741840f813ff18ca6d8dda4bd02
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-11 17:26:19 +0530

    Added Stock Acccount Management

---
+++ StockManagement/StockManagement/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_Inventory_Management
+{
+    /// <summary>
+    /// Model class for Data inventory
+    /// </summary>
+    public class Inventory
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Inventory> inventories { get; set; }
+    }
+}
---
+++ StockManagement/StockManagement/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Data_Inventory_Management
+{
+    class Program
+    {
+        /// <summary>
+        /// creating a json file and adding inventory to it and
+        /// reading json file and deserializing it to string format
+        /// also calculating the total value for inventory.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            DataInventortyManagement management = new DataInventortyManagement();
+            management.GetData();
+
+        }
+    }
+}
